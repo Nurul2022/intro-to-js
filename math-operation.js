8ea637831611fac31ed4fb26bce734bbfee70e1c
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-29 11:50:50 +0600

    first commit

---
+++ math-operation.js
@@ -0,0 +1,10 @@
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
---
+++ variable-name.js
@@ -0,0 +1,3 @@
+var name = 'Hero Alan'
+
+// reserved keyword not allowed
